3a19f3f0e4020cae9e6152d364aec95db11a4ea3
Jacob Wall <jacobrwall33@gmail.com>
2023-08-07 15:23:00 -0400

    rearrange file structure and add deploy script for github pages

---
+++ src/components/AppBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar class="grey-bottom-border" flat>
+    <template v-slot:prepend>
+      <v-img src="@/assets/sunny_cup.png" height="40" width="40"> </v-img>
+    </template>
+    <v-app-bar-title> {{ currentModule }}</v-app-bar-title>
+    <v-spacer />
+    <v-btn icon="mdi-account-search-outline" color="primary"> </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentModule: "Customer Dashboard",
+    };
+  },
+  methods: {
+    changeModule(name) {
+      this.currentModule = name;
+    },
+  },
+};
+</script>
